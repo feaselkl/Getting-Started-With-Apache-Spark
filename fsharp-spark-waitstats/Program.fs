3be2f5c5631e68d079345d37779e33843b9fd0fb
Kevin Feasel <feaselkl@gmail.com>
2019-10-04 23:03:01 -0400

    Adding F# example

---
+++ fsharp-spark-waitstats/Program.fs
@@ -0,0 +1,40 @@
+﻿open System
+open Microsoft.Spark.Sql
+
+[<EntryPoint>]
+let main argv =
+    let spark = SparkSession
+                    .Builder()
+                    .AppName("F# Wait Stats Demo")
+                    .GetOrCreate()
+    let dataFrame = spark.Read()
+                        .Option("inferSchema", true)
+                        .Csv(argv.[0])
+
+    // Demonstration 1:  using the DataFrame API.
+    printfn "Sum of milliseconds waiting where wait is over 10K MS in a time period, grouped by wait type."
+    dataFrame
+        .Where("MillisecondsWaiting > 10000")
+        .GroupBy("WaitType")
+        .Sum("MillisecondsWaiting")
+        .Show()
+
+    // Demonstration 2:  using Spark SQL.
+    printfn "Top 20 waits, grouped by wait type and event date."
+    dataFrame.CreateOrReplaceTempView("WaitStats")
+    spark.Sql("
+    SELECT
+        WaitType,
+        CAST(Time AS DATE) AS EventDate, 
+        SUM(MillisecondsWaiting) AS MillisecondsWaiting
+    FROM WaitStats
+    GROUP BY
+        WaitType,
+        CAST(Time AS DATE)
+    ORDER BY
+        MillisecondsWaiting DESC,
+        EventDate,
+        WaitType
+    LIMIT 20").Show(20, 0, true)
+
+    0 // return an integer exit code
